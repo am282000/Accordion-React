3f2ddfdd79a9e9ca5bf82ea4713e3d1dc7d79012
am282000 <madaansahaab777@gmail.com>
2021-06-09 12:24:46 +0530

    Done for today

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Accordion from "./component/accordion/Accordion";
+
+function App() {
+  return (
+    <>
+      < Accordion />
+     </>
+  );
+}
+
+export default App;
---
+++ src/component/accordion/Accordion.js
@@ -0,0 +1,22 @@
+import React,{useState} from 'react';
+import {questions} from './Api';
+import "./Accordion.css";
+import MyAccordion from './MyAccordion';
+
+const Accordion = () => {
+    const [data, setData] = useState(questions)
+    return (
+        <>
+            <section className="main-div">
+            <h1>React Interview Questions</h1>
+                {
+                    data.map((currElement)=>{
+                        return <MyAccordion key={currElement.id} {...currElement} />;
+                    })
+                }
+            </section>
+        </>
+    )
+}
+
+export default Accordion
